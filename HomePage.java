c8c161772a8b4c9ed283a8cf15ebf8409bd57883
akamisett <133157789+akamisett@users.noreply.github.com>
2023-05-10 12:18:00 -0700

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -10,14 +10,14 @@ public class HomePage extends JFrame implements ActionListener {
 
 	JButton startGame;
 	JButton rules;
+	JButton exit;
 	JPanel panel1;
 	JPanel panel2;
 	JLabel tle;
 	
 	public HomePage() {
-		
+		// TODO Auto-generated constructor stub
 		super("Checkers");
-		
 		startGame = new JButton("Start Game");
 		startGame.setActionCommand("start"); //set the command  
 		startGame.addActionListener(this);
@@ -28,6 +28,11 @@ public class HomePage extends JFrame implements ActionListener {
 		rules.addActionListener(this);
 		rules.setBounds(150, 175, 200, 50);
 		
+		exit = new JButton("Exit Game");
+		exit.setActionCommand("exit"); //set the command 
+		exit.addActionListener(this);
+		exit.setBounds(400, 250, 75, 20);
+		
 		tle = new JLabel("Checkers");
 		Font tleFont = new Font("Verdana", Font.BOLD, 30);
 		tle.setFont(tleFont);
@@ -35,15 +40,27 @@ public class HomePage extends JFrame implements ActionListener {
 				
 		setLayout(null);
         	setSize(500,300);
-       		setVisible(true);
+        	setVisible(true);
         
         	add(tle);
         	add(startGame);
         	add(rules);
+        	add(exit);
         
-        	getContentPane().setBackground(Color.red);
-		
-		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        	getContentPane().setBackground(Color.red);    
+       
+        
+	    	setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+	}
+	
+	public void actionPerformed( ActionEvent evt) {
+		if(evt.getActionCommand().equals("start")) {
+			System.out.println("Let the game begin");
+		} else if (evt.getActionCommand().equals("rules")) {
+			System.out.println("Here are the rules");
+		} else if(evt.getActionCommand().equals("exit")) {
+			System.exit(0);
+		}
 	}
 
 }
