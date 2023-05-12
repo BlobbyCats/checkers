dcd69b090d803fc846000482d440869a0ab0ecd1
Alex <ultimate0alex@gmail.com>
2023-05-12 11:12:49 -0700

    Added Colors class with updated Color values

---
+++ Colors.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+
+public class Colors {
+    public static final Color BLACK = new Color(0,0,0);
+    public static final Color RED = new Color(229,71,40);
+    public static final Color LIGHT_BROWN = new Color(230,188,131);
+    public static final Color DARK_BROWN = new Color(158,122,73);
+    public static final Color GREEN = new Color(51,108,51);
+}
