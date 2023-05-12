b245e28b13fc1421743038bcf09a87eb8851369e
uyen0130 <133155242+uyen0130@users.noreply.github.com>
2023-05-12 12:24:53 -0700

    Update Rule.java

--- Rule.java
+++ Rule.java
@@ -9,19 +9,18 @@ public class Rule extends JFrame implements ActionListener {
 	
     JLabel label= new JLabel();// display area of String of text
 
-    JFrame frame = new JFrame();// create Frameuy
 
 	public Rule() {
 		button.setBounds(20, 10, 100,30);
 		button.addActionListener(this);
 		
-		home.setBounds(570,10,100,30);
+		home.setBounds(870,10,100,30);
 		home.addActionListener(this);
 	
 		
        // label.setText(" Checkers is a board game played between 2 people on an 8x8 board. Each person had 12 pieces." );
         
-       label.setText("<html> <pre> 1. Checkers is a board game played between 2 people on an 8x8 board. "
+        label.setText("<html> <pre> 1. Checkers is a board game played between 2 people on an 8x8 board. "
         		+ "<pre> 2. Each person had 12 pieces." + " <pre> 3. Darker color goes first." + 
         		"<pre> 4. Pieces are always moved diagonally" + 
         		" <pre>    a. Pawns can only be moved \forward - diagonally <pre>" +
@@ -31,29 +30,38 @@ public class Rule extends JFrame implements ActionListener {
         		+ "7. If neither player has legal moves, then it is a draw <pre>"
         		+ "8. You win the game when the opponent has no more pieces or cannot move"
         		+ " </html>" );
+
         
-		label.setHorizontalAlignment(JLabel.CENTER);
 		
-        
-     	frame.setSize(800,500);//set the size of the frame
-     	frame.setTitle(" Rule Page ");
-     	frame.add(button);// add the button 
-     	frame.add(home);
-     	frame.add(label);// add label
-     	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit out of application 
-     	frame.setVisible(true);//make frame visible
+	label.setHorizontalAlignment(JLabel.CENTER);
+
+     	setSize(1000 ,600);//set the size of the frame
+        setTitle(" Rule Page ");
+     	getContentPane().setBackground(Colors.LIGHT_BROWN);
+     	add(button);// add the button 
+     	add(home);
+     	add(label);// add label
+     	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit out of application 
+     	setVisible(true);//make frame visible
      
 	}
 	public void actionPerformed(ActionEvent evt) {
 		// TODO Auto-generated method stub
-			if(evt.getActionCommand().equals("Home")) {
+	
+if(evt.getActionCommand().equals("Home")) {
+	HomePage hp= new HomePage();
 			
 		}
 		else {
-		
-		  }	
+			Game g= new Game();
+			
 		}
-		public static void main(String[] args) {
+      setVisible(false);
+			
+		}
+		
+		
+	public static void main(String[] args) {
 		new Rule();
 		
 	}
