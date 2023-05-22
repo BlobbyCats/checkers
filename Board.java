34ddf328d96bc4f4b327988cfb2649381496f5de
Ruthwika Gajjala <ruthwikagajjala@Ruthwikas-MacBook-Air.local>
2023-05-21 22:41:35 -0700

    fixed board

--- Board.java
+++ Board.java
@@ -374,14 +374,12 @@ public class Board extends JPanel implements MouseListener {
 					p2.incrementWins();
 					FinalScreen f = new FinalScreen(gameNum, p1.getWins(), p2.getWins(), p1.getName(), p2.getName(), whoWins, p1.getColor(), p2.getColor(), this);
 					game.setVisible(false);
-					this.setVisible(false);
 				}
 				else {
 					String whoWins = "p1";
 					p1.incrementWins();
 					FinalScreen f = new FinalScreen(gameNum, p1.getWins(), p2.getWins(), p1.getName(), p2.getName(), whoWins, p1.getColor(), p2.getColor(), this);
 					game.setVisible(false);
-					this.setVisible(false);
 				}
 			}
 		}
@@ -395,14 +393,12 @@ public class Board extends JPanel implements MouseListener {
 					p2.incrementWins();
 					FinalScreen f = new FinalScreen(gameNum, p1.getWins(), p2.getWins(), p1.getName(), p2.getName(), whoWins, p1.getColor(), p2.getColor(), this);
 					game.setVisible(false);
-					this.setVisible(false);
 				}
 				else {
 					String whoWins = "p1";
 					p1.incrementWins();
 					FinalScreen f = new FinalScreen(gameNum, p1.getWins(), p2.getWins(), p1.getName(), p2.getName(), whoWins, p1.getColor(), p2.getColor(), this);
 					game.setVisible(false);
-					this.setVisible(false);
 				}
 			}
 		}
